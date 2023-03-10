9d7eab6771ab6b11e39cf066078f834853dcca90
iamguiss <guizinhosilvasantos10@gmail.com>
2023-03-09 21:12:13 -0300

    Pasta Novas

---
+++ app/src/main/java/br/com/etecia/appescolar/MenuPrincpalActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appescolar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincpalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_princpal_layout);
+    }
+}
